84aaa5407abb105a70396dc54b16e71d07f423d1
BesedinEgor <74902393+BesedinEgor@users.noreply.github.com>
2021-08-22 00:05:50 +0300

    new site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     $('.gallery-carousel').slick({
+//         infinite: true,
+//         arrows: false,
+//         dots: true,
+//         // autoplay: true
+//     });
+// });
