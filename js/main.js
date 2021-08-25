db89b0b3081d3d4da928dfb3e4a72068648e8b92
BesedinEgor <74902393+BesedinEgor@users.noreply.github.com>
2021-08-25 23:05:04 +0300

    animation slider fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     $('.gallery-info').slick({
         arrows: true,
-        dots: false
-        // autoplay: true
+        dots: false,
+        fade: true,
+        speed: 800,
+        autoplay: true
     });
 });
